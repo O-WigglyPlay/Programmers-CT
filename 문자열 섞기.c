d7f73c15dc448f3f7a02fef1e34fffb9ad67223c
O-WigglyPlay <dhehfud00@naver.com>
2023-11-07 15:16:35 +0900

    돌려놓기..
    오류로 인해 돌려놓기..

--- 문자열 섞기.c
+++ 문자열 섞기.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 char* solution(const char* str1, const char* str2) {
